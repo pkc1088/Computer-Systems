ba7225180e11334fa79b35207288560ce4b797e8
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-04-26 20:32:51 +0900

    Add files via upload

---
+++ 04.26 CS1 Heap&Stack.cpp
@@ -0,0 +1,24 @@
+/*
+
+shell : OS에서 kernal 과 user 사이에 끼어서 user의 명령을 해석하고 그 처리결과를
+		뿌려주는 시스템 프로그램
+		
+telnet : TCP/IP에서의 응용프로토콜 중 하나, 원격지의 컴퓨터를 이용하는
+		 가상 단말 기능을 위한 프로토콜
+		 
+Heap : Dynamic Memory Allcation (malloc, free)
+		사용자가 직접 관리
+		메모리 크기 제한 x
+		상대적으로 느림
+		전역적 엑세스 가능
+
+Stack : Static Memory Allocation (지역변수, 함수, 매개변수)
+		LIFO
+		변수를 명시적 할당, 해제할 필요 x
+		매우 빠른 액세스
+		지역변수만 가능
+		메모리 크기 제한 
+
+
+
+*/
